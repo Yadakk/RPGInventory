5d554f068291cd34b8974571831d27c6420fcb5c
IvanvIvanv <148952411+IvanvIvanv@users.noreply.github.com>
2024-04-09 19:32:05 +0300

    The drop version

--- Assets/Scripts/Inventory/ItemVoid.cs
+++ Assets/Scripts/Inventory/ItemVoid.cs
@@ -8,6 +8,8 @@ public class ItemVoid : MonoBehaviour
 {
     private Button _button;
     private Transform _slot;
+    private Slot _slotComp;
+    private static ItemVoid _instance;
 
     #region Initter
     private static UnityEvent _initAll = new();
@@ -21,8 +23,11 @@ public class ItemVoid : MonoBehaviour
     }
     private void OnInitAll()
     {
+        _instance = this;
+
         _button = transform.GetChild(0).GetComponent<Button>();
         _slot = transform.GetChild(1);
+        _slotComp = _slot.GetComponent<Slot>();
 
         _button.onClick.AddListener(OnVoidItem);
     }
@@ -34,5 +39,11 @@ public class ItemVoid : MonoBehaviour
         {
             Destroy(_slot.GetChild(1).gameObject);
         }
+    }
+
+    public static void VoidItemStatic(GameObject obj)
+    {
+        _instance.OnVoidItem();
+        _instance._slotComp.Insert(obj);
     }
 }
